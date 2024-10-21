ea21463d3b304f5edee9600f9f8581cf6f9c99da
Otto Gaulke <gaulkeotto@gmail.com>
2024-10-21 08:56:23 -0500

    Update tcx_parser.py
    check .tcx's already parsed

--- tcx_parser.py
+++ tcx_parser.py
@@ -5,4 +5,11 @@ import tcxreader as tr
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 files = os.listdir(os.getcwd())
 
+if 'parse_history.txt' not in files:
+  with open('parse_history.txt', 'w') as f:
+    pass
 
+with open('parsed_history.txt', 'r') as f:
+  parsed_files = f.read().splitlines()
+
+files_parse = [x for x in files if x not in parsed_files and x[-4:] == '.tcx']

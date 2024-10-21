49a5a01f54aba958312fffdbe1fcd9b1517c4d59
Otto Gaulke <gaulkeotto@gmail.com>
2024-10-21 09:00:23 -0500

    Update tcx_parser.py
    create running_data.csv if does not exist and load it to df

--- tcx_parser.py
+++ tcx_parser.py
@@ -13,3 +13,9 @@ with open('parsed_history.txt', 'r') as f:
   parsed_files = f.read().splitlines()
 
 files_parse = [x for x in files if x not in parsed_files and x[-4:] == '.tcx']
+
+if 'running_data.csv' not in files:
+  df_empty = pd.DataFrame()
+  pd.to_csv('running_data.csv', index=False)
+
+df = pd.read_csv('running_data.csv')

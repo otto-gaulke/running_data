f59f21a0d06bbb16642f6c2db9c863b498257300
Otto Gaulke <gaulkeotto@gmail.com>
2024-10-21 19:03:42 -0500

    update

--- tcx_parser.py
+++ tcx_parser.py
@@ -3,27 +3,52 @@ import pandas as pd
 import tcxreader as tr
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-files = os.listdir(os.getcwd())
+os.chdir('..')
+host_files = os.listdir(os.getcwd())
 
-if 'parse_history.txt' not in files:
-  with open('parse_history.txt', 'w') as f:
+if 'parse_history.txt' not in host_files:
+  with open('parse_history.txt', 'w') as parse_write:
     pass
 
-with open('parsed_history.txt', 'r') as f:
-  parsed_files = f.read().splitlines()
+with open('parse_history.txt', 'r') as parse_read:
+  parsed_files = parse_read.read().splitlines()
 
-files_parse = [x for x in files if x not in parsed_files and x[-4:] == '.tcx']
+cols = ['date', 'time', 'latitude', 'longitude',
+        'elevation_meters', 'distance_meters', 'heart_rate']
 
-if 'running_data.csv' not in files:
-  df_empty = pd.DataFrame()
-  pd.to_csv('running_data.csv', index=False)
+if 'running_data.csv' not in host_files:
+  df = pd.DataFrame(columns=cols)
+else:
+  df = pd.read_csv('running_data.csv')
 
-df = pd.read_csv('running_data.csv')
+os.chdir(os.getcwd() + '\\tcx')
+tcx_files = os.listdir(os.getcwd())
+
+files_parse = [x for x in tcx_files if x not in parsed_files and x[-4:] == '.tcx']
 
 for parse in files_parse:
-  data = tr.TCXReader(parse).read()
+  data = tr.TCXReader().read(parse)
+  trackpoints = data.trackpoints
 
-  for workout in data.workouts():
-    for track in workout.track:
-      for point in track.track_points:
-        
+  date = []
+  time = []
+  latitude = []
+  longitude = []
+  elevation_m = []
+  distance_m = []
+  hr = []
+
+  for point in trackpoints:
+    date.append(str(point.time)[:10])
+    time.append(str(point.time)[11:19])
+    latitude.append(point.latitude)
+    longitude.append(point.longitude)
+    elevation_m.append(point.elevation)
+    distance_m.append(point.distance)
+    hr.append(point.hr_value)
+
+  elevation_ft = [x * 3.28084 for x in elevation_m]
+  distance_ft = [x * 3.28084 for x in distance_m]
+  distance_mi = [x * 0.000621371 for x in distance_m]
+
+x = 1
